b8786003f37914366e077b9476045821855bfa14
Bikendra Chaudhary <bikendrachaudhary98097@gmail.com>
2024-04-26 11:07:43 +0545

    modified sql file

--- CollegeMonitor/Data/book.sql
+++ CollegeMonitor/Data/book.sql
@@ -1,3 +1,3 @@
 -- SQLite
 INSERT INTO Courses (Id, Name, TextBook, CreditHrs)
-VALUES (1,Data Structure ', 'Introduction to Algorithms', 3)
+VALUES (1,'Data Structure ', 'Introduction to Algorithms', 3)
