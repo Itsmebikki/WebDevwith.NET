e363f542b8cbc862d87df054c4df95f01f14e855
Bikendra Chaudhary <bikendrachaudhary98097@gmail.com>
2024-05-09 10:42:14 +0545

    Created student page

---
+++ Vidhyalaya/Pages/Students/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vidhyalaya.Pages_Students
+{
+    public class EditModel : PageModel
+    {
+        private readonly VidhyalayaDbContext _context;
+
+        public EditModel(VidhyalayaDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student =  await _context.Students.FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            Student = student;
+           ViewData["GradeId"] = new SelectList(_context.Grades, "GradeId", "GradeId");
+           ViewData["GuardianId"] = new SelectList(_context.Guardians, "GuardianId", "GuardianId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(Student.StudentId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.StudentId == id);
+        }
+    }
+}
