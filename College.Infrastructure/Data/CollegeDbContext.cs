fd742320496def8320d8bdda2c19c8cb36bf637b
Bikendra Chaudhary <bikendrachaudhary98097@gmail.com>
2024-05-25 13:53:12 +0545

    Copied Data from collegeapi project

---
+++ College.Infrastructure/Data/CollegeDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+public class CollegeDbContext: DbContext 
+{
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Session> Sessions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CollegeMonitor;Trusted_Connection=True;");
+    } 
+}
